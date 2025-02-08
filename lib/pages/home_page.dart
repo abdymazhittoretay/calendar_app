a36709d4cedb1fb85a9a0cddc200379f24dff5cb
Toretay <abdymvzhit@gmail.com>
2025-02-08 23:13:58 +0500

    added basic table calendar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,10 +9,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final CalendarFormat _format = CalendarFormat.month;
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      body: SafeArea(
+        child: TableCalendar(
+          calendarFormat: _format,
+          focusedDay: _focusedDay,
+          firstDay: DateTime.utc(1980, 01, 01),
+          lastDay: DateTime.utc(2100, 12, 31),
+          onDaySelected: (selectedDay, focusedDay) {
+            if (!isSameDay(_selectedDay, selectedDay)) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            }
+          },
+          selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+          onPageChanged: (focusedDay) {
+            _focusedDay = focusedDay;
+          },
+        ),
+      ),
     );
   }
 }
