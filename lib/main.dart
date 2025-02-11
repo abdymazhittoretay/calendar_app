6a45141059dfd7074a086cd624bd8ffba4040b7b
Toretay <abdymvzhit@gmail.com>
2025-02-11 22:14:29 +0500

    created box to implement operations on database

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_app/boxes.dart';
 import 'package:calendar_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
@@ -5,6 +6,7 @@ import 'package:hive_flutter/adapters.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  calendarBox = await Hive.openBox("boxcalendar");
   runApp(const MyApp());
 }
 
